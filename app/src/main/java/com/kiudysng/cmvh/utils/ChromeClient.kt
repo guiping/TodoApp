5f03059c536680c3198a38171b65284297ac5291
guiping <gping.vip@gmail.com>
2023-12-07 15:36:40 +0800

    完善功能逻辑

--- app/src/main/java/com/kiudysng/cmvh/utils/ChromeClient.kt
+++ app/src/main/java/com/kiudysng/cmvh/utils/ChromeClient.kt
@@ -3,18 +3,18 @@ package com.kiudysng.cmvh.utils
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.net.http.SslError
 import android.os.Message
-import android.webkit.JsResult
-import android.webkit.WebChromeClient
-import android.webkit.WebView
+import android.util.Log
+import android.webkit.*
 import android.webkit.WebView.WebViewTransport
 
 
-class ChromeClients(var activity: Activity, var webView: WebView ) :
+class ChromeClients(var activity: Activity, var webView: WebView) :
     WebChromeClient() {
     private val TAB = "ChromeClients"
     override fun onJsAlert(view: WebView, url: String, message: String, result: JsResult): Boolean {
-               val builder = AlertDialog.Builder(activity)
+        val builder = AlertDialog.Builder(activity)
         builder.setMessage(message)
             .setPositiveButton("OK") { arg0: DialogInterface, arg1: Int -> arg0.dismiss() }.show()
         result.cancel()
@@ -28,6 +28,26 @@ class ChromeClients(var activity: Activity, var webView: WebView ) :
         resultMsg: Message
     ): Boolean {
         val newWebView = WebView(view.context)
+
+        newWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                Log.e("pLog", "请求地址 ---- ${request.url}")
+                WebUtils.openWebView(activity, request.url.toString())
+
+                return true
+            }
+
+            override fun onReceivedSslError(
+                view: WebView,
+                handler: SslErrorHandler,
+                error: SslError
+            ) {
+                handler.proceed()
+            }
+        }
         val transport = resultMsg.obj as WebViewTransport
         transport.webView = newWebView
         resultMsg.sendToTarget()
